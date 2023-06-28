1fba56e7356e7b9a7963a218bbde77f9b007413e
Duke 0 <innominate3301@proton.me>
2023-06-27 20:15:38 -0400

    working login and signup

--- backend/models/mongoose.js
+++ backend/models/mongoose.js
@@ -13,6 +13,8 @@ function setup(mongoose){
           lastName: { type: String, required: true },
           email: { type: String, required: true },
           password: { type: String, required: true },
+          birthday: { type: Date, required: true },
+          gender: { type: String, required: false }
         }
       );
 
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,14 @@ const app = express();
 const passport = require('passport');
 const { initialize } = require('./validators/passportConfig');
 const flash = require('connect-flash');
+const mongoose = require('mongoose');
+const { setup } = require('./models/mongoose');
 
-app.use(flash());
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/api',(req, res) => {
  res.json({'users': ['userOne', 'userTwo']});
@@ -17,8 +23,30 @@ app.listen(3002, () => [
 initialize(passport)
 
 app.post('/api/login', passport.authenticate("local", {
-    successRedirect: "/",
+    successRedirect: "/dashboard",
     failureRedirect: "/?msg=Login%20Failed",
 }),
 );
 
+app.get('/api/posts', async (req, res) => {
+    setup(mongoose)
+    const Post = mongoose.model('post')
+    const posts = await Post.find({})
+    res.json({posts})
+})
+
+app.post('/api/users', async (req, res) => {
+    setup(mongoose)
+    const User = mongoose.model('user')
+    const { email, password, birthday, gender, firstName, lastName } = req.body
+    const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        password,
+        birthday,
+        gender,
+    }) 
+    await newUser.save()
+    res.json({user: newUser})
+})
