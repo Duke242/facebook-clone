43c16b22d4b5e436d714ea70371c21471756a2de
Duke 0 <innominate3301@proton.me>
2023-06-29 15:36:56 -0400

    dashboard blueprint finished

--- frontend/src/Components/Feed.js
+++ frontend/src/Components/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Feed.css'
-import { FaPlus, FaUserAlt, FaVideo, FaPhotoVideo, FaSmile, FaCamera } from 'react-icons/fa'
+import { FaPlus, FaUserAlt, FaVideo, FaPhotoVideo, FaSmile, FaCamera,
+  FaEllipsisH, FaTimes, FaThumbsUp, FaComment, FaShare } from 'react-icons/fa'
 
 
 function Feed() {
@@ -17,7 +18,7 @@ function Feed() {
       </div>
       <div className='post-container'>
         <div className='post-container-top'>
-          <FaUserAlt size={40}/> 
+          <FaUserAlt size={40} color='#0092ED'/> 
           <span className='post-text-container'>
             What's on your mind, User?
           </span>
@@ -30,6 +31,37 @@ function Feed() {
       </div>
       <div className='post-container-room'>
         <button><FaCamera size={30}/>Create room</button>
+      </div>
+      <div className='other-post-container'>
+        <div className='other-post-container-author'>
+          <FaUserAlt size={35} color='#0092ED' />
+          <div class='other-post-name-time'>
+            <h5>author name</h5>
+            <p>time posted</p>
+          </div>
+          <div className='other-post-icons'>
+          <button><FaEllipsisH size={25} color='gray'/></button>
+          <button><FaTimes size={25} color='gray'/></button>
+          </div>
+        </div>
+        <div className='other-post-textarea'>
+          <p>Post text</p>
+        </div>
+        <div className='other-post-media'>
+          media
+        </div>
+        <div className='other-post-likes'>
+        <div>0 likes</div>
+        <div>
+          <span>0 comments</span>
+          <span>0 shares</span>
+        </div>
+        </div>
+        <div className='other-posts-buttons'>
+          <button><FaThumbsUp/>Like</button>
+          <button><FaComment/>Comments</button>
+          <button><FaShare/>Share</button>
+        </div>
       </div>
     </div>
   )
